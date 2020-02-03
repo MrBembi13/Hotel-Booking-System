1cb43e84b2090a8b14d1b4ae055a53086852b87d
Promethium14 <yasyulyanets@gmail.com>
2020-02-03 23:52:00 +0200

    added myBatis, Log4j support & project structure

---
+++ src/main/java/com/solvd/hotel_booking_system/Executor.java
@@ -0,0 +1,8 @@
+package com.solvd.hotel_booking_system;
+
+public class Executor {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/com/solvd/hotel_booking_system/util/LoggerUtil.java
@@ -0,0 +1,10 @@
+package com.solvd.hotel_booking_system.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerUtil {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+}
