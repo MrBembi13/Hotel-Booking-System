d5b0e90aeb96b87551e63c2a198961196f0fd02a
Promethium14 <yasyulyanets@gmail.com>
2020-02-07 17:19:18 +0200

    configuration for MyBatis, Log4j2

---
+++ src/main/java/com/solvd/hotel_booking_system/Executor.java
@@ -0,0 +1,10 @@
+package com.solvd.hotel_booking_system;
+
+import com.solvd.hotel_booking_system.util.LoggerUtil;
+
+public class Executor {
+
+    public static void main(String[] args) {
+        LoggerUtil.LOGGER.info("Starting application...");
+    }
+}
