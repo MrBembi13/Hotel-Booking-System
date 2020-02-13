2258bbc27b5417a475e4e15ab1b92418b7738539
MrBembi13 <vasylrudyk13@gmail.com>
2020-02-14 01:05:34 +0200

    some fixes

--- src/main/java/com/solvd/hotel_booking_system/dao/IBookingsDAO.java
+++ src/main/java/com/solvd/hotel_booking_system/dao/IBookingsDAO.java
@@ -3,11 +3,14 @@ package com.solvd.hotel_booking_system.dao;
 import com.solvd.hotel_booking_system.model.BookingsModel;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IBookingsDAO {
     BookingsModel getBookingsById(Long id);
 
     List<BookingsModel> getBookingsList();
+
+    List<BookingsModel> findByParameters(Map<String, Object> map);
 
     void insertBookings(BookingsModel entity);
 
