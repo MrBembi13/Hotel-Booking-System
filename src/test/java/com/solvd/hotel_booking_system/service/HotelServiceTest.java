702235ce3d9669aee0a5325bcfce85a8928c663d
MrBembi13 <vasylrudyk13@gmail.com>
2020-02-21 00:03:31 +0200

    fix writing mistakes

--- src/test/java/com/solvd/hotel_booking_system/service/HotelServiceTest.java
+++ src/test/java/com/solvd/hotel_booking_system/service/HotelServiceTest.java
@@ -1,11 +1,9 @@
 package com.solvd.hotel_booking_system.service;
 
-import com.solvd.hotel_booking_system.dao.daoClass.AddressDAO;
 import com.solvd.hotel_booking_system.dao.daoClass.HotelsDAO;
 import com.solvd.hotel_booking_system.model.AddressModel;
 import com.solvd.hotel_booking_system.model.HotelsModel;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -14,28 +12,17 @@ import java.util.Map;
 
 public class HotelServiceTest {
 
-    HotelsService hotelsService;
-    AddressModel addressModel;
-    HotelsModel hotelsModel;
-    AddressDAO addressDAO;
-    HotelsDAO hotelsDAO;
-
-    @BeforeMethod
-    public void init() {
-        hotelsService = new HotelsService();
-        addressModel = new AddressModel();
-        hotelsModel = new HotelsModel();
-        addressDAO = new AddressDAO();
-        hotelsDAO = new HotelsDAO();
-    }
-
     @Test
-    public void successfullyFindHotelsByCity() {
+    public void verifyFindHotelsByParameters() {
+        HotelsService hotelsService = new HotelsService();
+        AddressModel addressModel = new AddressModel();
+        HotelsModel hotelsModel;
+        HotelsDAO hotelsDAO = new HotelsDAO();
         hotelsModel = hotelsDAO.getHotelsById(1L);
         Map<String, Object> parametersMap = new HashMap<>();
         parametersMap.put("nameHotel", "hotel_name61");
         List<HotelsModel> actualList = hotelsDAO.findByParameters(parametersMap);
-        Assert.assertEquals(hotelsService.findByParameters(addressModel, hotelsModel), actualList);
+        Assert.assertEquals(hotelsService.findByParameters(addressModel, hotelsModel), actualList, "Method works");
 
     }
 }
--- src/test/java/com/solvd/hotel_booking_system/service/JsonTest.java
+++ src/test/java/com/solvd/hotel_booking_system/service/JsonTest.java
@@ -3,39 +3,28 @@ package com.solvd.hotel_booking_system.service;
 import com.solvd.hotel_booking_system.model.PaymentTypeModel;
 import com.solvd.hotel_booking_system.util.JSONUtil;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class JsonTest {
 
-    private JSONUtil<PaymentTypeModel> paymentTypeModelJSONUtil;
-    private PaymentTypeModel paymentTypeModel;
-    private PaymentTypeModel paymentTypeModel2;
-
-    @BeforeMethod
-    public void init() {
-        paymentTypeModelJSONUtil = new JSONUtil<>();
-        paymentTypeModel = new PaymentTypeModel();
-        paymentTypeModel2 = new PaymentTypeModel();
+    @Test
+    public void verifyParseFromJson() {
+        JSONUtil<PaymentTypeModel> paymentTypeModelJSONUtil = new JSONUtil<>();
+        PaymentTypeModel paymentTypeModel = paymentTypeModelJSONUtil.fromJson(PaymentTypeModel.class, "json\\PaymentType.json");
+        Assert.assertEquals("type1", paymentTypeModel.getType(), "correct parse");
+        Assert.assertEquals("description1", paymentTypeModel.getDescription(), "correct parse");
     }
 
     @Test
-    public void successfullyParseFromJsonById() {
-        paymentTypeModel = paymentTypeModelJSONUtil.fromJson(PaymentTypeModel.class, "json\\PaymentType.json");
-        Assert.assertEquals("type1", paymentTypeModel.getType());
-        Assert.assertEquals("description1", paymentTypeModel.getDescription());
-    }
-
-    @Test
-    public void successfullyParseToJsonById() {
+    public void verifyParseToJson() {
+        JSONUtil<PaymentTypeModel> paymentTypeModelJSONUtil = new JSONUtil<>();
+        PaymentTypeModel paymentTypeModel = new PaymentTypeModel();
+        PaymentTypeModel paymentTypeModel2;
         paymentTypeModel.setType("type2");
         paymentTypeModel.setDescription("description2");
         paymentTypeModelJSONUtil.toJson(paymentTypeModel,"json\\PaymentType.json");
         paymentTypeModel2 = paymentTypeModelJSONUtil.fromJson(PaymentTypeModel.class, "json\\PaymentType.json");
-        Assert.assertEquals(paymentTypeModel.getType(), paymentTypeModel2.getType());
-        Assert.assertEquals(paymentTypeModel.getDescription(), paymentTypeModel2.getDescription());
+        Assert.assertEquals(paymentTypeModel.getType(), paymentTypeModel2.getType(), "correct parse");
+        Assert.assertEquals(paymentTypeModel.getDescription(), paymentTypeModel2.getDescription(), "correct parse");
     }
 }
